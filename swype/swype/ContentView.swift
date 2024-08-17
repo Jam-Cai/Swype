0124a1708626df8fda305d54488cae0c6ca7dbe7
wqba <wabarurse@gmail.com>
2024-08-16 22:52:06 -0400

    created app project in xcode

---
+++ swype/swype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swype
+//
+//  Created by Richard Huang on 2024-08-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swype/swype/swypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swypeApp.swift
+//  swype
+//
+//  Created by Richard Huang on 2024-08-16.
+//
+
+import SwiftUI
+
+@main
+struct swypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
