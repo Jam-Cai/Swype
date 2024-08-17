4eae2607f9074f60f63557d7ccca8a360b876ef6
wqba <wabarurse@gmail.com>
2024-08-17 13:19:44 -0400

    save before i make a button

---
+++ swype/swype/Views/ClassLeaderboardView.swift
@@ -0,0 +1,18 @@
+//
+//  ClassLeaderboardView.swift
+//  swype
+//
+//  Created by Richard Huang on 2024-08-17.
+//
+
+import SwiftUI
+
+struct ClassLeaderboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClassLeaderboardView()
+}
